2bb725b293f4048c7cce7221fa96b06eabf7cad0
Kawa <kawago37@gmail.com>
2025-05-01 13:58:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function getRandomColor() {
+  // Generate random RGB values
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Return as RGB string
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+
 document.getElementById('b').addEventListener('click', function() {
   document.body.style.backgroundColor = getRandomColor();
 });
