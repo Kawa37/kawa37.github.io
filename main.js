5d3512c9d3098401ac6be0a8f56df8125f98f7fb
Kawa <kawago37@gmail.com>
2025-05-16 19:52:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,5 +29,5 @@ document.getElementById('timeBtn').addEventListener('click', function() {
 });
 
 document.getElementById('calcBtn').addEventListener('click', function() {
-  window.location.href = 'https://kawa37.github.io/pwd-generator'; // Navigates to the time page
+  window.location.href = 'https://kawa37.github.io/calc'; // Navigates to the time page
 });
