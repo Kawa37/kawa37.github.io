fbe2f2083a2deed6219dba4c9f6463b3b93e36e2
Kawa <kawago37@gmail.com>
2026-01-15 21:30:50 +0200

    Update shanqman.js

--- shanqman.js
+++ shanqman.js
@@ -18,6 +18,14 @@ for (let i = 0;i<randomWord.length;i++){
     spaces.push('_')
 }
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+document.body.style.backgroundColor = getRandomColor();
 spacesDisplay.textContent = spaces.join(' ')
 let tries = 7
 document.getElementById('tries').textContent = `Tries: ${tries}`
@@ -57,6 +65,7 @@ document.getElementById('guessBtn').addEventListener('click', function() {
     }
     document.getElementById('input').value = '';
 });
+
 
 
 
